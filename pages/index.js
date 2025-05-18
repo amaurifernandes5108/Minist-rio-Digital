183d523e94d23f134984089efbcb80a738922425
amaurifernandes5108 <amaurifernandes1975@gmail.com>
2025-05-17 21:30:06 -0300

    Versão inicial do Ministério Digital do Pastor Amauri

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const BioDesigner = () => {
+  // Cole aqui o código completo do seu componente Bio Designer,
+  // incluindo todos os estados, funções e retorno de JSX
+}
+
+export default BioDesigner;
